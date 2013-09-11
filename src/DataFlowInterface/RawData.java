222dc0accc6177a20a3d0e6035c4b9bd108f7b76
sjain0@walmartlabs.com <siddhanth.iitb@gmail.com>
2013-09-11 21:36:04 +0530

    interface for processed and raw data

---
+++ src/DataFlowInterface/RawData.java
@@ -0,0 +1,40 @@
+package DataFlowInterface;
+
+/**
+ * Raw data class which can include all the data before the data has been
+ * processed
+ * 
+ * @author sjain7
+ * 
+ */
+public class RawData {
+
+	private String url;
+	private String question;
+	private String answer;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+}
